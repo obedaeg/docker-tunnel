5ce19e935c3e885d3d863182c6eb294b695738b6
Obed Espinoza <obed.espinoza@healthcare.com>
2020-09-30 17:38:55 -0600

    feat(entry): fix entry point to be compatible with windows

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-autossh -N -M 0 -o StrictHostKeyChecking=false -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -4 -L 0.0.0.0:$LOCAL_PORT:$FORWARD_HOST:$FORWARD_PORT -i $KEYFILE $TUNNEL_USER@$TUNNEL_HOST
+cp $KEYFILE /keyfiles/mykey.pem
+chmod 400 /keyfiles/mykey.pem
+autossh -N -M 0 -o StrictHostKeyChecking=false -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -4 -L 0.0.0.0:$LOCAL_PORT:$FORWARD_HOST:$FORWARD_PORT -i /keyfiles/mykey.pem $TUNNEL_USER@$TUNNEL_HOST
